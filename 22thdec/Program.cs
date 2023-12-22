15ec59548f466c533818121c20a21fdd39b758c9
Nishchitha L R <nishunishugowda0@gmail.com>
2023-12-22 15:20:43 +0530

    Add project files.

---
+++ 22thdec/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22thdec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;//initial value
+            int y = 30;
+
+            x = x + y;
+            y = x - y;
+            x = x - y;
+
+            //output the swapped values
+            Console.WriteLine($"X ={x}");
+            Console.WriteLine($"Y ={y}");
+        }
+    }
+}
